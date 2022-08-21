6686eb3db381cfedd7b8e86cc8ebcf4ca55ce274
excelolatunbosun <excellenceolatunbosun@betpawa.com>
2022-08-21 13:12:32 +0100

    primitive types and reference types

--- src/com/codewithexcel/Main.java
+++ src/com/codewithexcel/Main.java
@@ -1,7 +1,16 @@
 package com.codewithexcel;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args ){
         byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
